ebe2f6fcb1881da50f42b90be22a8b8e68af132e
tammarabm <165035795+tammarabm@users.noreply.github.com>
2024-10-21 18:48:43 -0300

    aplicando bootstrap a la pantalla de resultados

--- src/components/PantallaResultado.jsx
+++ src/components/PantallaResultado.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import { Button } from 'react-bootstrap';
 
 const PantallaResultado = ({ isCorrect, nextRound }) => (
-  <div>
-    <h2>{isCorrect ? "Correcto!" : "Incorrecto!"}</h2>
-    <button onClick={nextRound}>Siguiente desafío</button>
-  </div>
+  <div className="d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
+    <Card className="text-center" style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>{isCorrect ? "¡Correcto!" : "¡Incorrecto!"}</Card.Title>
+        <Button variant="success" onClick={nextRound}> Siguiente desafío</Button>
+      </Card.Body>
+    </Card>
+  </div> 
+
 );
 
 export default PantallaResultado;
