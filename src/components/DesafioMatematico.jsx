2b31c187cc620341d542fa9008fd93b3cd2da3e0
Leonardo Rafael Guanuco <leomusicocompositor@gmail.com>
2024-10-21 22:38:39 -0300

    Mejorando el css

--- src/components/DesafioMatematico.jsx
+++ src/components/DesafioMatematico.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Button } from 'react-bootstrap';
 
 const DesafioMatematico = ({ challenge, verifyAnswer }) => {
   const [userAnswer, setUserAnswer] = useState('');
@@ -10,13 +11,20 @@ const DesafioMatematico = ({ challenge, verifyAnswer }) => {
 
   return (
     <div>
-      <h2>Resuelve: {challenge.num1} {challenge.operator} {challenge.num2}</h2>
-      <input 
-        type="number" 
-        value={userAnswer} 
-        onChange={(e) => setUserAnswer(e.target.value)} 
-      />
-      <button onClick={handleSubmit}>Verificar resultado</button>
+      <div className="container text-center text-bg-success p-3">
+        <h1>DESAFIOS</h1>
+      </div>
+      <h2 className='text-center mt-md-5'>Resuelve: {challenge.num1} {challenge.operator} {challenge.num2}</h2>
+      <div className='d-flex justify-content-center mt-md-5'>
+        <input
+          type="number"
+          value={userAnswer}
+          onChange={(e) => setUserAnswer(e.target.value)}
+        />
+      </div>
+      <div className='d-flex justify-content-center mt-md-5'>
+        <Button onClick={handleSubmit}>Verificar resultado </Button>
+      </div>
     </div>
   );
 };
